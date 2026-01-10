d8ad74991d150d774ff9326041713d585a5b83b4
Abdelrahman Khaled <kabdelrahman996@gmail.com>
2026-01-11 01:20:25 +0200

    created .env file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://itqiueefnfycygbuzlhq.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0cWl1ZWVmbmZ5Y3lnYnV6bGhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ1ODMzMzQsImV4cCI6MjA4MDE1OTMzNH0.mb88aBt5AId-VBu_piC-_Dvkd8UATBzE84q5TbKF6LM";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
